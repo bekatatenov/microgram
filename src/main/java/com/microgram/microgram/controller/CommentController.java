90812d49d55645441eff34e7ba42403374c79f3a
Beksultan Tatenov <bekatatenov@gmail.com>
2021-01-28 17:10:08 +0600

    [+] Функция добавления новой публикаций (картинки и текстового описания). Пользователь может загрузить любое количество публикаций. [+] Удаление публикаций. Пользователь может удалить только свою публикацию. При удалении публикации так же удаляются все связанные с ней комментарии. [+] Добавление комментариев к своей или чужой публикации. Пользователь оставляет комментарии к понравившимся ему публикациям. [+]Удаление комментариев. Можно удалить любые комментарии, но только под своей публикацией. [+] Иметь возможность подписываться на других пользователей, что бы видеть их публикации у себя в ленте.

--- src/main/java/com/microgram/microgram/controller/CommentController.java
+++ src/main/java/com/microgram/microgram/controller/CommentController.java
@@ -1,4 +1,43 @@
 package com.microgram.microgram.controller;
 
+import com.microgram.microgram.dto.CommentDto;
+import com.microgram.microgram.exception.ResourceNotFoundException;
+import com.microgram.microgram.models.User;
+import com.microgram.microgram.services.CommentService;
+import com.microgram.microgram.services.LikeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
+    @Autowired
+    CommentService commentService;
+
+    @Autowired
+    LikeService likeService;
+
+
+    @PostMapping(path = "/{postId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public CommentDto addComment(@RequestBody CommentDto commentData,
+                                 @PathVariable String postId) {
+        User user = commentData.getUser();
+
+        boolean thereAnyLikeByUserAndPost = likeService.isThereAnyLikeByUserAndPost(postId, user.getId());
+        if (!thereAnyLikeByUserAndPost) {
+            throw new ResourceNotFoundException("There is no like in this post by user");
+        }
+
+        return commentService.addComment(commentData, postId, user.getId());
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<Void> deletePostById(@PathVariable String commentId) {
+        if (commentService.deleteComment(commentId)) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
--- src/main/java/com/microgram/microgram/controller/SubscriptionController.java
+++ src/main/java/com/microgram/microgram/controller/SubscriptionController.java
@@ -1,4 +1,19 @@
 package com.microgram.microgram.controller;
 
+import com.microgram.microgram.dto.SubscriptionDto;
+import com.microgram.microgram.services.SubscriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/subs")
 public class SubscriptionController {
+    @Autowired
+    SubscriptionService subscriptionService;
+
+    @PostMapping("/{id}")
+    public SubscriptionDto addSubs(@PathVariable String id, @RequestParam("userID") String userId) {
+        return subscriptionService.addSubscription(userId, id);
+    }
 }
